dacd68846ec44e1c26c9713a81730e10a24c5135
Piers Warmers <hello@pierswarmers.com>
2010-03-23 14:17:26 +1100

    Included new partial in show template.

--- modules/gnWikiPage/templates/showSuccess.php
+++ modules/gnWikiPage/templates/showSuccess.php
@@ -1,16 +1,3 @@
 <?php include_partial('use') ?>
 <?php include_partial('tools', array('gn_wiki_page' => $gn_wiki_page)) ?>
-<?php $primary_image = $gn_wiki_page->getFirstImage(); ?>
-
-<h1<?php echo  !is_null($gn_wiki_page->getDeletedAt())? ' class="deleted"' : ''?>><?php echo $gn_wiki_page->getTitle() ?></h1>
-
-<?php echo markdown_to_html($gn_wiki_page->getContent(), $gn_wiki_page); ?>
-
-<dl class="gn-meta-data">
-  <dt><?php echo __('Created') ?>:</dt>
-  <dd><?php echo $gn_wiki_page->getCreatedAt() ?></dd>
-  <dt><?php echo __('Updated') ?>:</dt>
-  <dd><?php echo $gn_wiki_page->getUpdatedAt() ?></dd>
-  <dt><?php echo __('Version') ?>:</dt>
-  <dd><?php echo link_to($gn_wiki_page->getVersion(), 'gnWikiPage/versions?id='.$gn_wiki_page->getId()) ?></dd>
-</dl>
+<?php include_partial('wiki_page', array('gn_wiki_page' => $gn_wiki_page, 'sf_cache_key' => $gn_wiki_page->getId())) ?>
