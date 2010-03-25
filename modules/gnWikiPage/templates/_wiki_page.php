628c5cc73803474b67ce13786009b94dfc6e92d3
Piers Warmers <hello@pierswarmers.com>
2010-03-26 00:29:12 +1100

    Added some new classes for formating reasons.

--- modules/gnWikiPage/templates/_wiki_page.php
+++ modules/gnWikiPage/templates/_wiki_page.php
@@ -1,6 +1,8 @@
 <h1<?php echo  !is_null($gn_wiki_page->getDeletedAt())? ' class="deleted"' : ''?>><?php echo $gn_wiki_page->getTitle() ?></h1>
 
+<div class="gn-page-content clearfix">
 <?php echo markdown_to_html($gn_wiki_page->getContent(), $gn_wiki_page); ?>
+</div>
 
 <dl class="gn-meta-data">
   <dt><?php echo __('Created') ?>:</dt>
--- modules/gnWikiPage/templates/showSuccess.php
+++ modules/gnWikiPage/templates/showSuccess.php
@@ -1,3 +1,5 @@
 <?php include_partial('use') ?>
 <?php include_partial('tools', array('gn_wiki_page' => $gn_wiki_page)) ?>
+<div class="gn-wiki-page">
 <?php include_partial('wiki_page', array('gn_wiki_page' => $gn_wiki_page, 'sf_cache_key' => $gn_wiki_page->getId())) ?>
+</div>
