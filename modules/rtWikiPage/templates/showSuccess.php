d10931932bffd14c806d899e270741f7a0c93a07
Piers Warmers <hello@pierswarmers.com>
2010-06-10 09:52:28 +1000

    Added frontend edit button system.

--- modules/rtWikiPage/templates/showSuccess.php
+++ modules/rtWikiPage/templates/showSuccess.php
@@ -1,5 +1,7 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate') ?>
-<div class="rt-wiki-page-show">
+
+<div class="rt-wiki-page-show rt-admin-edit-tools-panel">
+  <?php echo link_to(__('Edit'), 'rtWikiPageAdmin/edit?id='.$rt_wiki_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
   <h1><?php echo $rt_wiki_page->getTitle() ?></h1>
   <div class="rt-page-content clearfix">
   <?php echo markdown_to_html($rt_wiki_page->getContent(), $rt_wiki_page); ?>
