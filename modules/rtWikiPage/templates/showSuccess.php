e35c2660af88e53e4067dcbbcfe90ca33c07fb68
Torsten Hirsch <torsten@wranglers.com.au>
2010-06-22 14:02:44 +1000

    add naming convention to class.

--- modules/rtWikiPage/templates/showSuccess.php
+++ modules/rtWikiPage/templates/showSuccess.php
@@ -1,6 +1,6 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate') ?>
 
-<div class="rt-wiki-page-show rt-admin-edit-tools-panel">
+<div class="rt-wiki-page rt-show rt-admin-edit-tools-panel">
   <?php echo link_to(__('Edit'), 'rtWikiPageAdmin/edit?id='.$rt_wiki_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
   <h1><?php echo $rt_wiki_page->getTitle() ?></h1>
   <div class="rt-page-content clearfix">
