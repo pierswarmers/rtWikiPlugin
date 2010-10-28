3059326163405fb7ceaf5119d3c3d14ca7790d4d
Piers Warmers <hello@pierswarmers.com>
2010-10-29 08:36:38 +1100

    Ugraded handling of headings and div structure.

--- modules/rtWikiPage/templates/_wiki_page.php
+++ modules/rtWikiPage/templates/_wiki_page.php
@@ -1,2 +1,6 @@
-<h1><?php echo $rt_wiki_page->getTitle() ?></h1>
+<?php use_helper('rtText') ?>
+<?php slot('rt-title') ?>
+<?php echo $rt_wiki_page->getTitle() ?>
+<?php end_slot(); ?>
+
 <?php echo markdown_to_html($rt_wiki_page->getContent(), $rt_wiki_page); ?>
--- modules/rtWikiPage/templates/showSuccess.php
+++ modules/rtWikiPage/templates/showSuccess.php
@@ -1,9 +1,4 @@
-<?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate') ?>
+<?php use_helper('I18N') ?>
 
-<div class="rt-wiki-page rt-show rt-primary-container rt-admin-edit-tools-panel">
-  <?php echo link_to(__('Edit'), 'rtWikiPageAdmin/edit?id='.$rt_wiki_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
-  <h1><?php echo $rt_wiki_page->getTitle() ?></h1>
-  <div class="rt-container">
-    <?php echo markdown_to_html($rt_wiki_page->getContent(), $rt_wiki_page); ?>
-  </div>
-</div>
+<?php echo link_to(__('Edit'), 'rtWikiPageAdmin/edit?id='.$rt_wiki_page->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
+<?php include_partial('wiki_page', array('rt_wiki_page' => $rt_wiki_page, 'sf_cache_key' => $rt_wiki_page->getId())) ?>
